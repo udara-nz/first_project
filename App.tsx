cde4d8f44cf7f9330d79311bdf20ddf1f556e06b
UdaraPc <udara.vidusl@gmail.com>
2023-12-14 16:57:30 +0530

    useState hook

--- App.tsx
+++ App.tsx
@@ -1,13 +1,40 @@
-import {Button, Linking, StyleSheet, Text, View} from 'react-native';
-import React from 'react';
+import {Button, StyleSheet, Text, View} from 'react-native';
+import React, {useState} from 'react';
 
 const App = () => {
+  const [name, setName] = useState('My First App Container is: Neuron');
+  const [date, setDate] = useState('Published in 2023-12-01');
+  const [current, setCurrent] = useState(true);
+  const [number, setNumber] = useState(0);
+  const [age, setAge] = useState(20231213);
+
+  const onClickHandler = () => {
+    setName('The second app will be continued...');
+    setDate('Approximately 2024-02-10');
+    setCurrent(false);
+  };
+
+  const onClicHandler = () => {
+    setNumber(number + 1);
+  };
+
+  const onCal = () => {
+    setAge(age - 19930819);
+  };
+
   return (
     <View style={styles.Container}>
-      <Text style={styles.TextContainer}>My First App Container</Text>
-      <Button
-        title="State Update"
-        onPress={() => Linking.openURL('https://www.youtube.com/')}></Button>
+      <Text style={styles.TextContainer}>{name}</Text>
+      <Text style={styles.TextContainer}>{date}</Text>
+      <Text style={styles.TextContainer}>
+        {current ? 'AI App' : 'Suggest the name'}
+      </Text>
+      <Button title="State Update" onPress={onClickHandler}></Button>
+      <Text style={styles.TextContainer}>{number * 5}</Text>
+      <Button title="ADD" onPress={onClicHandler}></Button>
+      <Text style={styles.TextContainer}>You clicked {number} times</Text>
+      <Text style={styles.TextContainer}>{age}</Text>
+      <Button title="Calculate" onPress={onCal}></Button>
     </View>
   );
 };
@@ -20,9 +47,9 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   TextContainer: {
-    margin: 20,
+    margin: 10,
     color: '#fff',
-    fontSize: 30,
+    fontSize: 20,
     fontStyle: 'italic',
   },
 });
