26413d29cc412eaba4da3920eb2ed3fd46d86c6a
UdaraPc <udara.vidusl@gmail.com>
2023-12-22 13:47:46 +0530

    state_hook

--- App.tsx
+++ App.tsx
@@ -1,108 +1,55 @@
-import {StyleSheet, Text, View} from 'react-native';
-import React from 'react';
+import {Button, StyleSheet, Text, View} from 'react-native';
+import React, {useState} from 'react';
 
 const App = () => {
+  const [name, setName] = useState('My First App Container is: Neuron');
+  const [date, setDate] = useState('Published in 2023-12-01');
+  const [current, setCurrent] = useState(true);
+  const [number, setNumber] = useState(0);
+  const [age, setAge] = useState(20231213);
+
+  const onClickHandler = () => {
+    setName('The second app will be continued...');
+    setDate('Approximately 2024-02-10');
+    setCurrent(false);
+  };
+
+  const onClicHandler = () => {
+    setNumber(number + 1);
+  };
+
+  const onCal = () => {
+    setAge(age - 19930819);
+  };
+
   return (
-    <View style={styles.body}>
-      <View style={styles.row}>
-        <View style={styles.view1}>
-          <Text style={styles.text}>1</Text>
-        </View>
-        <View style={styles.view2}>
-          <Text style={styles.text}>2</Text>
-        </View>
-        <View style={styles.view3}>
-          <Text style={styles.text}>3</Text>
-        </View>
-      </View>
-      <View style={styles.row}>
-        <View style={styles.view4}>
-          <Text style={styles.text}>4</Text>
-        </View>
-      </View>
-      <View style={styles.row}>
-        <View style={styles.view5}>
-          <Text style={styles.text}>5</Text>
-        </View>
-      </View>
-      <View style={styles.big_row}>
-        <View style={styles.view6}>
-          <Text style={styles.text}>6</Text>
-        </View>
-        <View style={styles.view7}>
-          <Text style={styles.text}>7</Text>
-        </View>
-      </View>
+    <View style={styles.Container}>
+      <Text style={styles.TextContainer}>{name}</Text>
+      <Text style={styles.TextContainer}>{date}</Text>
+      <Text style={styles.TextContainer}>
+        {current ? 'AI App' : 'Suggest the name'}
+      </Text>
+      <Button title="State Update" onPress={onClickHandler}></Button>
+      <Text style={styles.TextContainer}>{number * 5}</Text>
+      <Button title="ADD" onPress={onClicHandler}></Button>
+      <Text style={styles.TextContainer}>You clicked {number} times</Text>
+      <Text style={styles.TextContainer}>{age}</Text>
+      <Button title="Calculate" onPress={onCal}></Button>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  body: {
+  Container: {
     flex: 1,
-    flexDirection: 'column',
-    backgroundColor: '#bccefb',
-    alignItems: 'center',
-    justifyContent: 'flex-start',
-  },
-  row: {
-    flexDirection: 'row',
+    backgroundColor: 'black',
     alignItems: 'center',
     justifyContent: 'center',
   },
-  big_row: {
-    flex: 1,
-    flexDirection: 'row',
-    alignItems: 'stretch',
-    justifyContent: 'center',
-  },
-  view1: {
-    flex: 1,
-    backgroundColor: '#91a1e9',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  view2: {
-    flex: 2,
-    flexDirection: 'row',
-    backgroundColor: '#88bf8d',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  view3: {
-    flex: 3,
-    flexDirection: 'row',
-    backgroundColor: '#ffa7b6',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  view4: {
-    flex: 2,
-    backgroundColor: '#ff7373',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  view5: {
-    flex: 2,
-    backgroundColor: '#bf94e4',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  view6: {
-    flex: 1,
-    backgroundColor: '#88bf8d',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  view7: {
-    flex: 1,
-    backgroundColor: '#ec9118',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text: {
-    fontSize: 40,
-    fontWeight: '500',
+  TextContainer: {
+    margin: 10,
+    color: '#fff',
+    fontSize: 20,
     fontStyle: 'italic',
   },
 });
